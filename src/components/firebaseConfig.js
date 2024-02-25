f64451cdbbef940c10bd989d1fc3f83a67d799bb
Agustinbustos99 <143135335+Agustinbustos99@users.noreply.github.com>
2024-02-25 14:10:21 -0300

    final

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -5,14 +5,15 @@ import { getFirestore } from "firebase/firestore"; // Importa getFirestore
 
 // Configuración de tu aplicación de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBvshdNrpeXB6ieaCxIz63v9kfMFMSIxeM",
-  authDomain: "ecommerce-2b09a.firebaseapp.com",
-  projectId: "ecommerce-2b09a",
-  storageBucket: "ecommerce-2b09a.appspot.com",
-  messagingSenderId: "425018347753",
-  appId: "1:425018347753:web:15a955b6b1f32c51fae078",
-  measurementId: "G-SC0WQ4LN84"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
